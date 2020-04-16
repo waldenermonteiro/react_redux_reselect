db1cdc0c2b8793f41e13dfeda9e14345be2c4e77
Waldener Monteiro <jjrr019@gmail.com>
2020-04-16 05:54:57 -0300

    feat: create app component and use reselect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { createSelector } from "reselect";
+const App = ({ cart, total, addProducts, setShipping }) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Carrinho</h1>
+      <p>
+        Items: <strong>{cart.items.length}</strong>
+      </p>
+      <p>
+        Frete: <strong>{cart.shipping_value}</strong>
+      </p>
+      <p>
+        Total: <strong>{total}</strong>
+      </p>
+      <button onClick={addProducts}>Adicionar Produto</button>
+      <button onClick={setShipping}>Calcular Frete</button>
     </div>
   );
-}
+};
+const calculateTotal = createSelector(
+  (state) => state.items,
+  (items) => {
+    console.log('CALCULOU')
+    return items.reduce((subtotal, item) => subtotal + item.price, 0);
+  }
+);
 
-export default App;
+const mapStateToProps = (state) => ({
+  cart: state,
+  total: calculateTotal(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addProducts: () => dispatch({ type: "ADD" }),
+  setShipping: () => dispatch({ type: "SET_SHIPPING" }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
